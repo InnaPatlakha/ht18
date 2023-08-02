3855a65abbe8ed6d3b61a8921d2674bfda94b6b5
Inna Patlakha <i.inna1903@gmail.com>
2023-08-03 00:37:54 +0300

    task2 done

--- task2.js
+++ task2.js
@@ -2,3 +2,34 @@
 У змінній znak може бути: +, -, *, /, %, ^ (ступінь ).
 Вивести результат математичної дії, вказаної в змінній znak.
 Обидва числа і знак виходять від користувача.*/
+
+function doMath(x, znak, y) {
+  x = Number(prompt("Введіть перше число"));
+  y = Number(prompt("Введіть друге число"));
+  znak = prompt("Введіть бажану операцію: +, -, *, /, %, ^ ");
+  let result;
+
+  switch (znak) {
+    case "+":
+      result = x + y;
+      break;
+    case "-":
+      result = x - y;
+      break;
+    case "*":
+      result = x * y;
+      break;
+    case "/":
+      result = x / y;
+      break;  
+    case "%":
+      result = x % y;
+      break;
+    case "^":
+      result = Math.pow(x,y);
+      break;    
+  }
+  return result;
+}
+
+alert(doMath());
